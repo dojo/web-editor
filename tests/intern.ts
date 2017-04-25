0b177037e2f1d0961175287443cf1acb8d3d39ff
Kitson Kelly <me@kitsonkelly.com>
2017-04-25 22:14:01 +0100

    Fix excludeInstrumentation to exclude external bundles

--- tests/intern.ts
+++ tests/intern.ts
@@ -45,4 +45,4 @@ export const suites = [ '@dojo/test-extras/support/loadJsdom', 'tests/unit/all' 
 
 export const functionalSuites = [ 'tests/functional/all' ];
 
-export const excludeInstrumentation = /(?:node_modules|tests)[\/\\]/;
+export const excludeInstrumentation = /(?:node_modules|tests|external)[\/\\]/;
