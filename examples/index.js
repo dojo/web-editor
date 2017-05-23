da6cd8d36d4afb0ba7dc498597bf3fde8b293517
Kitson Kelly <me@kitsonkelly.com>
2017-05-23 19:27:23 +0100

    Changes from theme branch

--- examples/index.js
+++ examples/index.js
@@ -8,16 +8,52 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "@dojo/core/lang", "@dojo/widget-core/d", "@dojo/widget-core/mixins/Projector", "@dojo/widget-core/WidgetBase", "../Editor", "../project", "../Runner"], factory);
+        define(["require", "exports", "@dojo/core/lang", "@dojo/widget-core/d", "@dojo/widget-core/mixins/Projector", "@dojo/widget-core/WidgetBase", "../Editor", "../project", "../Runner", "../support/themes"], factory);
     }
 })(function (require, exports) {
     "use strict";
+    var _this = this;
     Object.defineProperty(exports, "__esModule", { value: true });
     var lang_1 = require("@dojo/core/lang");
     var d_1 = require("@dojo/widget-core/d");
@@ -26,8 +62,10 @@ var __extends = (this && this.__extends) || (function () {
     var Editor_1 = require("../Editor");
     var project_1 = require("../project");
     var Runner_1 = require("../Runner");
+    var themes_1 = require("../support/themes");
     /* path to the project directory */
-    var PROJECT_DIRECTORY = '../projects/';
+    var PROJECT_DIRECTORY = '../../../projects/';
+    var theme;
     /**
      * An example application widget that incorporates both the Editor and Runner widgets into a simplistic UI
      */
@@ -145,7 +183,7 @@ var __extends = (this && this.__extends) || (function () {
                     },
                     key: 'wrap'
                 }, [
-                    d_1.w(Editor_1.default, { filename: this._editorFilename, key: 'editor' }),
+                    d_1.w(Editor_1.default, { filename: this._editorFilename, key: 'editor', options: { theme: theme } }),
                     d_1.w(Runner_1.default, runnerProperties)
                 ])
             ]);
@@ -154,7 +192,17 @@ var __extends = (this && this.__extends) || (function () {
     }(WidgetBase_1.default));
     /* Mixin a projector to the App and create an instance */
     var projector = new (Projector_1.default(App))();
-    /* Start the projector and append it to the document.body */
-    projector.append();
+    (function () { return __awaiter(_this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, themes_1.load('../themes/dojo2.json')];
+                case 1:
+                    theme = _a.sent();
+                    /* Start the projector and append it to the document.body */
+                    projector.append();
+                    return [2 /*return*/];
+            }
+        });
+    }); })();
 });
 //# sourceMappingURL=index.js.map
