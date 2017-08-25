84096f84efd33bf07009b74ca7a063a0f90d715e
Kitson Kelly <me@kitsonkelly.com>
2017-08-25 11:00:46 +0100

    Update intern config for @dojo/i18n support

--- tests/intern.ts
+++ tests/intern.ts
@@ -36,13 +36,24 @@ export const loaderOptions = {
 	packages: [
 		{ name: '@dojo', location: 'node_modules/@dojo' },
 		{ name: 'dojo', location: 'node_modules/intern/browser_modules/dojo' },
+		{ name: 'cldr-data', location: 'node_modules/cldr-data' },
+		{ name: 'cldrjs', location: 'node_modules/cldrjs' },
+		{ name: 'globalize', location: 'node_modules/globalize', main: 'dist/globalize' },
 		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette.min' },
 		{ name: 'pepjs', location: 'node_modules/pepjs/dist', main: 'pep' },
 		{ name: 'src', location: 'dev/src' },
 		{ name: 'sinon', location: 'node_modules/sinon/pkg', main: 'sinon' },
 		{ name: 'source-map', location: 'node_modules/source-map/dist', main: 'source-map.debug' },
 		{ name: 'tests', location: 'dev/tests' }
-	]
+	],
+	map: {
+		globalize: {
+			'cldr': 'cldrjs/dist/cldr',
+			'cldr/event': 'cldrjs/dist/cldr/event',
+			'cldr/supplemental': 'cldrjs/dist/cldr/supplemental',
+			'cldr/unresolved': 'cldrjs/dist/cldr/unresolved'
+		}
+	}
 };
 
 export const suites = [ '@dojo/shim/Promise', '@dojo/test-extras/support/loadJsdom', 'tests/unit/all' ];
